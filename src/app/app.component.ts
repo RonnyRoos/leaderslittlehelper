e5006e7b01aa93e3c49d6e0ff590e238571d8c17
Ronny Roos <ronny@ronnyroos.com>
2023-07-03 15:13:23 +0200

    Fix styling and script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,7 +63,6 @@ export class AppComponent {
     const window_width = $(window).width();
     let $sidebar = $('.sidebar');
     let $sidebar_responsive = $('body > .navbar-collapse');
-    let $sidebar_img_container = $sidebar.find('.sidebar-background');
 
 
     if (window_width && window_width > 767) {
@@ -85,8 +84,6 @@ export class AppComponent {
     });
 
     $('.fixed-plugin .badge').click(function () {
-      let $full_page_background = $('.full-page-background');
-
 
       $(this).siblings().removeClass('active');
       $(this).addClass('active');
@@ -101,49 +98,10 @@ export class AppComponent {
         $sidebar_responsive.attr('data-color', new_color);
       }
     });
-
-    $('.fixed-plugin .img-holder').click(function () {
-      let $full_page_background = $('.full-page-background');
-
-      $(this).parent('li').siblings().removeClass('active');
-      $(this).parent('li').addClass('active');
-
-
-      var new_image = $(this).find("img").attr('src');
-
-      if ($sidebar_img_container.length != 0) {
-        $sidebar_img_container.fadeOut('fast', function () {
-          $sidebar_img_container.css('background-image', 'url("' + new_image + '")');
-          $sidebar_img_container.fadeIn('fast');
-        });
-      }
-
-      if ($full_page_background.length != 0) {
-
-        $full_page_background.fadeOut('fast', function () {
-          $full_page_background.css('background-image', 'url("' + new_image + '")');
-          $full_page_background.fadeIn('fast');
-        });
-      }
-
-      if ($sidebar_responsive.length != 0) {
-        $sidebar_responsive.css('background-image', 'url("' + new_image + '")');
-      }
-    });
   }
 
   ngAfterViewInit() {
     this.runOnRouteChange();
-  }
-
-  isMaps(path: string) {
-    var titlee = this.location.prepareExternalUrl(this.location.path());
-    titlee = titlee.slice(1);
-    if (path == titlee) {
-      return false;
-    } else {
-      return true;
-    }
   }
 
   runOnRouteChange(): void {
