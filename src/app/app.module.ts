f6f3fd82b54ffc41a2180255e25bf736442f046c
Ronny Roos <ronny@ronnyroos.com>
2023-07-03 15:36:33 +0200

    Remove material ui dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
-import { MatDialogModule } from "@angular/material/dialog";
 import { FormsModule } from "@angular/forms";
-import { MatInputModule } from "@angular/material/input";
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { AppRoutingModule } from "./app-routing.module";
 import { CommonModule } from "@angular/common";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatRippleModule } from "@angular/material/core";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatSelectModule } from "@angular/material/select";
-import { NavbarComponent } from "./core/navbar/navbar.component";
-import { FooterComponent } from "./core/footer/footer.component";
-import { SidebarComponent } from "./core/sidebar/sidebar.component";
 import { CoreModule } from "./core/core.module";
 
 @NgModule({
@@ -30,21 +17,10 @@ import { CoreModule } from "./core/core.module";
     AppComponent
   ],
   imports: [
-
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatRippleModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatTooltipModule,
-    MatInputModule,
     FormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
